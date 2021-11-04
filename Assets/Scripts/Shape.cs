7e5cbf4d5f2b5adcf2744d819812d8f7a3145089
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2021-11-04 18:09:10 +0100

    Arreglada rotacion y pasados toques a variables globales

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,53 +4,55 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour {
 
-    private Touch touch;
+    private Touch touchZero;
+    private Touch touchOne;
+
     private Quaternion rotationX;
     private Quaternion rotationY;
-    private float rotateSpeedModifier = .1f;
+    private Quaternion rotationZ;
+
+    private readonly float rotateSpeedModifier = .1f;
 
     public float maxZoom = 5f;
+    public float minZoom = 1f;
 
     void Update() {
         if (Input.touchCount == 1) {
-            touch = Input.GetTouch(0);
-            if (touch.phase.Equals(TouchPhase.Moved)) {
+            touchZero = Input.GetTouch(0);
+            if (touchZero.phase.Equals(TouchPhase.Moved)) {
                 RotateShape();
             }
         } else if (Input.touchCount == 2) {
-            Zoom(CalculatePinchDifference() * .01f);
-            //CalculatePinchRotation();
+            touchZero = Input.GetTouch(0);
+            touchOne = Input.GetTouch(1);
+            RotateShape();
+            Zoom(CalculatePinchDifference() * .0075f);
         }
     }
 
     void RotateShape() {
-        rotationX = Quaternion.Euler(touch.deltaPosition.y * rotateSpeedModifier, 0f, 0f);
-        rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0f);
+        rotationX = Quaternion.Euler(touchZero.deltaPosition.y * rotateSpeedModifier, 0f, 0f);
+        rotationY = Quaternion.Euler(0f, -touchZero.deltaPosition.x * rotateSpeedModifier, 0f);
+        rotationZ = Quaternion.Euler(0f, 0f, touchOne.deltaPosition.x * rotateSpeedModifier);
+
         transform.rotation = rotationY * transform.rotation;
         transform.rotation = rotationX * transform.rotation;
+        transform.rotation = rotationZ * transform.rotation;
     }
 
     void Zoom(float increment) {
-        Vector3 newScale = new Vector3(
-            Mathf.Clamp(transform.localScale.x + increment, 1f, maxZoom),
-            Mathf.Clamp(transform.localScale.y + increment, 1f, maxZoom),
-            Mathf.Clamp(transform.localScale.z + increment, 1f, maxZoom)
-        );
-        transform.localScale = newScale;
+        transform.localScale = ClampScale(increment);
     }
 
-    void CalculatePinchRotation() {
-        Touch touchZero = Input.GetTouch(0);
-        Touch touchOne = Input.GetTouch(1);
-
-        Quaternion rotationFromTouchToTouch = Quaternion.FromToRotation(touchZero.position, touchOne.position);
-        transform.localRotation = transform.localRotation * Quaternion.Inverse(rotationFromTouchToTouch);
+    Vector3 ClampScale(float increment) {
+        return new Vector3(
+            Mathf.Clamp(transform.localScale.x + increment, minZoom, maxZoom),
+            Mathf.Clamp(transform.localScale.y + increment, minZoom, maxZoom),
+            Mathf.Clamp(transform.localScale.z + increment, minZoom, maxZoom)
+        );
     }
 
     float CalculatePinchDifference() {
-        Touch touchZero = Input.GetTouch(0);
-        Touch touchOne = Input.GetTouch(1);
-
         Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
         Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
